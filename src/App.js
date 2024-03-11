1229a07e40b144bcba82aa35546e5f9a087fb814
vidyashankargs93 <vidyashankargs93@gmail.com>
2024-03-11 12:42:55 +0530

    add project to git repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Title from './Components/Title';
 
 function App() {
+
+  const [name, setName] = useState('mario');
+  const [showEvt, setShowEvt] = useState(true);
+  const [evt, setEvts] = useState([
+    { name: "Harry", key: 1 },
+    { name: "Hari", key: 2 },
+    { name: "Ron", key: 3 }])
+
+  const handleClick = (id) => {
+    console.log(id);
+    setEvts((prevEvent) => {
+      return prevEvent.filter((event) => {
+        return id !== event.key;
+      })
+    });
+
+  }
+
+  const handleShowEvt = () => {
+    setShowEvt(!showEvt);
+    console.log(showEvt);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title/>
+      {!showEvt && (<button onClick={handleShowEvt}>Show Events</button>)}
+      {showEvt && (<button onClick={handleShowEvt}>Hide events</button>)}
+      {showEvt && evt.map((event, idx) => (
+        <div key={event.key}>Key : {event.key} Index : {idx} Name: {event.name}
+          <button onClick={() => handleClick(event.key)}>Delete</button>
+        </div>
+      ))}
     </div>
   );
 }
---
+++ src/Components/Title.js
@@ -0,0 +1,13 @@
+function Title(){
+    return(
+        <div>
+            <h1 className="title">Welcome to Mario</h1>
+            <h3 className="subtitle">& Luigi's family</h3>
+        </div>
+
+    )
+
+
+}
+
+export default Title;
